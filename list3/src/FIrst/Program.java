c2c5bf9370e65bd4131275cb2e21ad25eccde60b
Artemeow <43747062+Artemeow@users.noreply.github.com>
2018-10-11 23:22:07 +0300

    Add files via upload

--- list3/src/FIrst/Program.java
+++ list3/src/FIrst/Program.java
@@ -3,19 +3,13 @@ package FIrst;
 public class Program {
 	
 	public static void main(String[] args) {
-		
-		
+
 		Text text = new Text("... Позволяющую получить текст в виде строки и вернуть массив слов.");
 		
-		StringToArray sta = new StringToArray();
+		String[] words = text.getText().split(" ");
 		
-		String[] res = sta.getArray(text);
-		
-		for(String re : res)
-	    System.out.println(re);
-		
+		for(String word : words) {
+	    System.out.println(word);
+		}
 	}
-
-	
-	
 }
